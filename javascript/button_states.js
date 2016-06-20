25d56ed1c90cadc4c20ec34e46e6ebf862adf84a
ethanny2 <ethanny2@gmail.com>
2016-06-20 19:54:40 -0400

    Button Pulls work, next is to add  varibles to the functions to further abstractify and be able to use the function for to generate any button

--- javascript/button_states.js
+++ javascript/button_states.js
@@ -8,6 +8,7 @@
 var allPlatforms  = {};
 var platformOffSetCounters = 0; 
 
+
 function showResults(result){
 	var plaformButtonList = $('.button_column.platform ul');
 	var defaultAmountButtons = 6;
@@ -24,40 +25,27 @@ function showResults(result){
 			button.text(platformName);
 			plaformButtonList.append(button);
 			buttonsOnScreen++;
-
 			//Default is 6 buttons 
 			if(buttonsOnScreen>defaultAmountButtons){
 				button.hide();
 			}
 			//Finally since the api limits the amount of results to 100 at a time.
 			//Create an offeset counter, to put up where you left off and 
-
+			platformOffSetCounters++;
+			console.log('offset is currently at ' + platformOffSetCounters);	
 		});
 	console.log('done WITH showResults() function');
 	console.log(allPlatforms);
-
+	
 }
 
-
-function createIntialButtons(){
-			//var plaformButtonList = $('.button_column.platform ul');
-			//var button1 = $('#first');
-			//var button2 = $('#second');
-			//var button3 =  $('#third');
-			//button1.attr('id', )
-			//button
-}
 
 
 
 
 //Api Pull function needs to be its only function, not annoyomous. 
-
-
-
-
- $(document).ready(function(){
- 		console.log("executing json Giant bomb api pull");	
+function pullPlatformData(){
+	console.log('offset is currently at ' + platformOffSetCounters);
 		$.ajax({
  		url: 'http://www.giantbomb.com/api/platforms/?api_key=f04a7c4e6c0dc84173ec15ce40ca61d964d4b5b0&format=jsonp&callback=?&sort=release_date:desc&field_list=name,release_date',
         type: "GET",
@@ -69,7 +57,7 @@ function createIntialButtons(){
         		 crossDomain:true,
         		 offset: platformOffSetCounters,
         		// field_list:'name,release_date',
-        		 json_callback:'showResults',
+        		 json_callback:'showResults',	
         		// sort:'release_date',
         	},
         dataType:'jsonp'	
@@ -78,6 +66,16 @@ function createIntialButtons(){
  			showResults(data.results);
  			console.log(data);
  		});
+}
+
+
+
+
+//If offset counter is not a multiple of 100, then you reached the end of the list.
+ $(document).ready(function(){
+ 		console.log("executing json Giant bomb api for the first time.");
+ 		pullPlatformData();	
+ 		
  });
 
 
@@ -94,6 +92,12 @@ function createIntialButtons(){
 				//it should do maybe 6 at a time.
 				//All the buttons are premade loop through the collumn
 				//and make 3 buttons unhidden at a time.
+				console.log(platformOffSetCounters);
+				if( platformOffSetCounters%100 == 0 || platformOffSetCounters==0 ){
+					console.log('pulling platform data');
+					pullPlatformData();
+				}
+
 			var plaformButtonArray = $('.button_column.platform .game_button');
 			var amountOfButtonsGenerate = 3;
 			//Change counter to change how many buttons come up.
@@ -111,9 +115,7 @@ function createIntialButtons(){
 				}	
 			});
 
-
-
-			// var  button = $("<button> </button>");
+				// var  button = $("<button> </button>");
 			// button.addClass('game_button');
 			// button.attr('id','fourth' );
 			// button.text('fourth');
