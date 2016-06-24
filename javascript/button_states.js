da7f391ef7d4bcdf23986e053517571b5a8e865b
ethanny2 <ethanny2@gmail.com>
2016-06-24 19:16:36 -0400

    Got the genre section for the button working, list of bugs also introduced 1. document.ready function does not run code sequentially, there for the two ajax calls happen at the same time meaning that the what gets put into the columns might be messed up 2) Hitting the generate button when there is an offset makes more buttons than expected be revealed. 3) The ajax call is failing, but it takes out data

--- javascript/button_states.js
+++ javascript/button_states.js
@@ -3,127 +3,247 @@
 //NEEDS .on();
 //http://www.giantbomb.com/api/platforms/?api_key=f04a7c4e6c0dc84173ec15ce40ca61d964d4b5b0&format=jsonp&callback=?&sort=release_date&field_list=name,release_date
 //Results limited to 100 at a time use offset=100 to get more.
+// http://www.giantbomb.com/api//?api_key=f04a7c4e6c0dc84173ec15ce40ca61d964d4b5b0&format=jsonp&callback=?&field_list=name';
+// http://www.giantbomb.com/api/company/?api_key=f04a7c4e6c0dc84173ec15ce40ca61d964d4b5b0&format=jsonp&callback=?&field_list=name';
+
+//BUG! PULLDATA ACTIVATES SHOW RESULTS WHICH ADDS DEFAULT BUTTONS TO THE SCREEN, BUT GENEREATE BUTTONS CALLS IT AGAIN, SO IT GENEATES
+//MORE BUTTONS THAN EXPECTED.
+//DOCUMENT.READY DOES NOT RUN SEQUENTIALLY 
 
 
-var allPlatforms  = {};
+var allPlatforms  = [];
 var platformOffSetCounters = 0; 
+var genreOffSetCounters = 0;
+var scoreOffSetCounters = 0;
+var timeOffSetCounters = 0;
+//Booleans to see which column to append the buttons to
+var platformSwitch=false ;
+var genreSwitch=false ;
+var scoreSwitch = false; 
+var timeSwitch=false; 
+var showButtons = 4;
+var defaultAmountButtons =6; //bUTTONS to initially spawned
 
+
+//var platformUrl = 'http://www.giantbomb.com/api/platforms/?api_key=f04a7c4e6c0dc84173ec15ce40ca61d964d4b5b0&format=jsonp&callback=?&sort=release_date:desc&field_list=name,release_date';
+var platformUrl = 'http://www.giantbomb.com/api/platforms/?api_key=f04a7c4e6c0dc84173ec15ce40ca61d964d4b5b0&sort=release_date:desc&field_list=name,release_date';
+var genreUrl = 'http://www.giantbomb.com/api/genres/?api_key=f04a7c4e6c0dc84173ec15ce40ca61d964d4b5b0&format=jsonp&callback=?&field_list=name';
+var scoreUrl ; //Hardcoded 
+var timeUrl  ;
 
 function showResults(result){
-	var plaformButtonList = $('.button_column.platform ul');
-	var defaultAmountButtons = 6;
 	var buttonsOnScreen = 0;
 	console.log('in the show results section');
 	//result.results is because there is some metadata that you need to cycle through
 	$.each(result.results, function(index,value) {
-			var platformName = value.name;
-			var releaseDate = value.release_date;
-			allPlatforms[platformName] = releaseDate;
+			var mainName = value.name;
 			var button =  $('<button></button>');
 			button.addClass('game_button');
-			button.attr('id', platformName);
-			button.text(platformName);
-			plaformButtonList.append(button);
+			button.attr('id', mainName);
+			button.text(mainName);
+			//Append here
+			var column ;
+			if(platformSwitch){
+				platformOffSetCounters++;
+				column = $('.button_column.platform');
+				column.append(button);
+				console.log('column of type platform '  + ' is at offset value '  +  platformOffSetCounters);
+
+			}else if (timeSwitch){
+				timeOffSetCounters++;
+				column = $('.button_column.time');
+				column.append(button);
+				console.log('column of type platform '  + ' is at offset value '  +  platformOffSetCounters);
+
+
+			}else if (genreSwitch){
+				genreOffSetCounters++;
+				column = $('.button_column.genre');
+				column.append(button);
+				console.log('column of type platform '  + ' is at offset value '  +  platformOffSetCounters);
+
+			}else if(scoreSwitch){
+				scoreOffSetCounters++;
+				column = $('.button_column.score');
+				column.append(button);
+				console.log('column of type platform '  + ' is at offset value '  +  platformOffSetCounters);
+			}
+
 			buttonsOnScreen++;
-			//Default is 6 buttons 
 			if(buttonsOnScreen>defaultAmountButtons){
+				console.log('HIDING BUTTON ');
 				button.hide();
 			}
-			//Finally since the api limits the amount of results to 100 at a time.
-			//Create an offeset counter, to put up where you left off and 
-			platformOffSetCounters++;
-			console.log('offset is currently at ' + platformOffSetCounters);	
+
 		});
 	console.log('done WITH showResults() function');
-	console.log(allPlatforms);
-	
+	//Turn off all the switches 
+	switchOffAll();
 }
 
 
+function switchOffAll(){
+		console.log('inside switch of all');
+		console.log('platformSwitch  is ' + platformSwitch );
+		console.log('genreSwitch  is ' + genreSwitch );
+		console.log('timeSwitch  is ' + timeSwitch );
+		console.log('scoreSwtich  is ' + scoreSwitch );
+		timeSwitch=false; 
+		genreSwitch=false;
+		scoreSwitch =false;
+		platformSwitch  = false;
+}
 
-
-
-//Api Pull function needs to be its only function, not annoyomous. 
-function pullPlatformData(){
-	console.log('offset is currently at ' + platformOffSetCounters);
+function pullData(url, offSetNum,type){
+	console.log('offset is currently at ' + offSetNum);
+		 switch(type){
+		 	case 'platform':
+		 		platformSwitch = true; 
+		 		break;
+		 	case 'genre':
+		 		genreSwitch = true; 
+		 		break;
+		 	case 'time':
+		 		timeSwitch = true; 
+		 		break;
+		 	case 'score':
+		 		scoreSwitch = true; 
+		 		break;
+		 }
 		$.ajax({
- 		url: 'http://www.giantbomb.com/api/platforms/?api_key=f04a7c4e6c0dc84173ec15ce40ca61d964d4b5b0&format=jsonp&callback=?&sort=release_date:desc&field_list=name,release_date',
+ 		url: url,
         type: "GET",
-        //What gets put inside the url.. the resources and api_ key dont work so I just hardcoded them in there...
+        dataType:'jsonp',
         data:{
-        		 //resources:'platforms',
-        		// api_key:'f04a7c4e6c0dc84173ec15ce40ca61d964d4b5b0',
         		 format:'jsonp',
         		 crossDomain:true,
-        		 offset: platformOffSetCounters,
-        		// field_list:'name,release_date',
-        		 json_callback:'showResults',	
-        		// sort:'release_date',
-        	},
-        dataType:'jsonp'	
- 		}).done(function(data){
- 			console.log('done with the pull of the api data');
- 			showResults(data.results);
- 			console.log(data);
- 		});
+        		 offset: offSetNum,
+        		 json_callback: 'showResults',	
+        	}
+ 		}).done(function()  {
+    		alert("Success.");
+		}).fail(function()  {
+    		alert("Sorry. Server unavailable. ");
+			}); 
+		
 }
 
 
 
 
-//If offset counter is not a multiple of 100, then you reached the end of the list.
  $(document).ready(function(){
  		console.log("executing json Giant bomb api for the first time.");
- 		pullPlatformData();	
+ 		//Pull all general starting with Platform
+ 		pullData(platformUrl, platformOffSetCounters,'platform');
+ 	//	pullData(genreUrl, genreOffSetCounters, 'genre');	
+ 		//defaultAmountButtons = 0 ;
+ 		
  		
  });
 
+ 
+
+//There should be 4 different generate buttons. 
+//1.Decide if what category the button is in
+//2. the annyomous function inside click, make it a real function.
+//3. pass in the url etc as need to the pull data function. 
+ $(function detectClick(){
 
 
+			$(".gen_button.platform").click(function(){
+				console.log('generate button clicked');
+				generateButtons(platformUrl, platformOffSetCounters, 'platform');
+				platformSwitch=true;
+			});
+			$(".gen_button.genre").click(function(){
+				console.log('generate button clicked');
+				generateButtons(genreUrl, genreOffSetCounters, 'genre');	
+				genreSwitch = true;
+			});
+			$(".gen_button.time").click(function(){
+				console.log('generate button clicked');
+				generateButtons(timeUrl, timeOffSetCounters, 'time');
+				timeSwitch = true;
 
+			});
+			$(".gen_button.score").click(function(){
+				console.log('generate button clicked');
+				generateButtons(scoreUrl, scoreOffSetCounters, 'score');
+				scoreSwitch = true;
+		
+			});
+			
 
-
-
-
- $(function generateButtons(){
-			$(".gen_button_platform").click(function(){
-				//Create new buttons in the platform section
-				//Right now it generates one button, but realistical
-				//it should do maybe 6 at a time.
-				//All the buttons are premade loop through the collumn
-				//and make 3 buttons unhidden at a time.
-				console.log(platformOffSetCounters);
-				if( platformOffSetCounters%100 == 0 || platformOffSetCounters==0 ){
-					console.log('pulling platform data');
-					pullPlatformData();
-				}
-
-			var plaformButtonArray = $('.button_column.platform .game_button');
-			var amountOfButtonsGenerate = 3;
-			//Change counter to change how many buttons come up.
-			var counter = 0;
-			$.each(plaformButtonArray, function(index,value){
-				var tempButton = $(value);
-				//console.log(tempButton);
-				if(counter<amountOfButtonsGenerate){
-						 if(tempButton.is( ":hidden" )){
-						 	console.log(tempButton);
-						 	tempButton.show();
-						 	counter++;	
-						 }
 					
+						
+
+
+
+				// if( platformOffSetCounters%100 == 0 || platformOffSetCounters==0 ){
+				// 	console.log('pulling platform data');
+				// 	pullPlatformData();
+				// }
+
+			//var plaformButtonArray = $('.button_column.platform .game_button');
+			//var amountOfButtonsGenerate = 3;
+			//Change counter to change how many buttons come up.
+		//	var counter = 0;
+			// $.each(plaformButtonArray, function(index,value){
+			// 	var tempButton = $(value);
+			// 	//console.log(tempButton);
+			// 	if(counter<amountOfButtonsGenerate){
+			// 			 if(tempButton.is( ":hidden" )){
+			// 			 	console.log(tempButton);
+			// 			 	tempButton.show();
+			// 			 	counter++;	
+			// 			}
+			// 	}	
+			// });		
+			
+		});
+
+ function generateButtons(url , offSetNum , type){
+ 					console.log('in function GENERATEBUTTONS()');
+					if( offSetNum%100 == 0 || offSetNum==0 ){
+						console.log('pulling more data');
+						pullData(url, offSetNum, type);
+					}	
+
+					var column ;
+					if(platformSwitch){
+						column = $('.button_column.platform .game_button');
+						console.log('buttons attempted to be added to platform ');
+					}else if (timeSwitch){
+						column = $('.button_column.time .game_button');
+						console.log('buttons attempted to be added to time ');
+
+					}else if (genreSwitch){
+						column = $('.button_column.genre .game_button');
+						console.log('buttons attempted to be added to genre ');
+
+					}else if(scoreSwitch){
+						column = $('.button_column.score .game_button ');
+						console.log('buttons attempted to be added to score ');
+
+					}
+					var counter = 0;
+					$.each(column, function(index,value){
+						console.log('entering the each function to make the buttons unhidden');
+						var tempButton = $(value);
+						console.log(tempButton);
+						if(counter<showButtons){
+							console.log('inside the show buttons unhiddens!!!!');
+							 if(tempButton.is( ":hidden" )){
+							 	console.log('counter is at ' + counter);
+							 	tempButton.show();
+							 	counter++;		
+							 }
 				}	
 			});
+			switchOffAll();
+ }
 
-				// var  button = $("<button> </button>");
-			// button.addClass('game_button');
-			// button.attr('id','fourth' );
-			// button.text('fourth');
 
-			// console.log('created button with  class ' + button.attr('class'));
-			// plaformButtonList.append(button);
-			});
-		});
 
 //Changing the button states
 $(document).on('click' , '.game_button' , function(){
